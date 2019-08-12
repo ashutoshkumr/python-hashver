b1a70170a7759c4e1f9d7fa876fbc064e46d8bda
Ashutosh Kumar <ashutoshk.akumar@gmail.com>
2019-08-13 01:28:50 +0530

    do not repost to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hashver",
-    version="0.0.1",
+    version="0.0.2",
     author="Ashutosh Kumar",
     author_email="ashutoshk.akumar@gmail.com",
     description=(
